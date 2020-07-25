ec4a37e61c8743ff6379bb497d65b384372cd8dc
Shreyans13 <50544190+Shreyans13@users.noreply.github.com>
2020-07-25 23:17:21 +0530

    Last Digit of the Sum of Fibonacci Numbers solution added

--- Assignment 2/2-6 Last Digit of the Sum of Fibonacci Numbers.cpp
+++ Assignment 2/2-6 Last Digit of the Sum of Fibonacci Numbers.cpp
@@ -1,25 +1,34 @@
-# include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
-long long FibRecurs(long long n) {
-	long long v[n] = {0};
+long long get_fibonacci_last_digit_fast(long long n) {
+    if (n <= 1)
+        return n;
+
+    long long v[n] = {0};
 
 	v[0] = 0;
 	v[1] = 1;
 
-	for ( long long i = 2 ;i <= n ;i++){
+	for ( long i = 2 ;i <= n ;i++){
 		v[i] = (v[i-1] + v[i-2]) % 10;
 	}
 
 	return v[n];
 }
 
-int main () {
-	long long n;
-	cin >> n;
-	if(n == 0){
-        cout << 0 << endl;
-	} else {
-        cout << FibRecurs(n) << endl;
-	}
+int fibonacci_sum_fast(long long n) {
+    int new_n = (n + 2) % 60;
+    int new_last = get_fibonacci_last_digit_fast(new_n);
+    if (new_last == 0) {
+        return 9;
+    } else {
+        return new_last - 1;
+    }
+}
+
+int main() {
+    long long n = 0;
+    cin >> n;
+    cout << fibonacci_sum_fast(n);
 }
